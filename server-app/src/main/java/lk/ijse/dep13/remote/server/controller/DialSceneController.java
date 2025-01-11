010bbb60d5b845f7b583b79878f3671a4377fc56
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-01-12 04:13:49 +0530

    Design UI

---
+++ server-app/src/main/java/lk/ijse/dep13/remote/server/controller/DialSceneController.java
@@ -0,0 +1,39 @@
+package lk.ijse.dep13.remote.server.controller;
+
+import javafx.animation.ScaleTransition;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+public class DialSceneController {
+    public ImageView imgDial;
+    public ImageView imgVideo;
+    public AnchorPane root;
+
+    public void imgOnMouseClicked(MouseEvent event) {
+        
+    }
+
+    public void imgOnMouseEntered(MouseEvent event) {
+        ScaleTransition ft = new ScaleTransition( Duration.millis(200), (ImageView) event.getTarget());
+        ft.setFromX(1);
+        ft.setFromY(1);
+        ft.setToX(1.1);
+        ft.setToY(1.1);
+        ft.playFromStart();
+    }
+
+    public void imgOnMouseExited(MouseEvent event) {
+        ScaleTransition ft = new ScaleTransition(Duration.millis(200), (ImageView) event.getTarget());
+        ft.setFromX(1.1);
+        ft.setFromY(1.1);
+        ft.setToX(1);
+        ft.setToY(1);
+        ft.playFromStart();
+    }
+
+
+}
---
+++ shared-app/src/main/java/lk/ijse/dep13/remote/shared/util/Routes.java
@@ -0,0 +1,5 @@
+package lk.ijse.dep13.remote.shared.util;
+
+public enum Routes {
+    MAIN
+}
